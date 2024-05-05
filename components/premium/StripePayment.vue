b56a8f6f123c24c8a337621025cfa4d36594c1e4
HugoMichard <hugomichard@yahoo.com>
2024-05-05 15:42:01 +0200

    fix payment button

---
+++ components/premium/StripePayment.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <button
+      class="px-3 py-3 w-full rounded-lg font-semibold text-sm text-white bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-700 duration-150"
+      @click="subscribe"
+      :disabled="props.is_active"
+    >
+      {{ props.is_active ? 'Subscribed' : 'Subscribe' }}
+    </button>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  subscription: {
+    type: Object,
+    required: true
+  },
+  is_active: {
+    type: Boolean,
+    required: true
+  }
+})
+
+const subscribe = () => {
+  navigateTo('/app/login')
+}
+</script>
+
+<style>
+.stripe-card {
+  min-width: 350px; /* Minimum width for each div */
+  flex: 1; /* Distribute available space evenly among the divs */
+}
+</style>

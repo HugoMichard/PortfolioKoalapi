e79e62cf9d7e591ecedd7dce68bb7d6b44a862f1
HugoMichard <hugomichard@yahoo.com>
2024-05-05 14:13:17 +0200

    Updates

---
+++ _nuxt/2Wcli3Yc.js
@@ -0,0 +1,1 @@
+import{r as e}from"./DM3dEHR9.js";const o=e(""),a=e(""),u=async n=>{const r=await fetch(n+".md").then(function(t){return t.text()}).then(function(t){return t});o.value=r,a.value=n};function i(){return{getPageMarkdown:u,pathToFile:a,viewedPageMarkdown:o}}export{i as u};

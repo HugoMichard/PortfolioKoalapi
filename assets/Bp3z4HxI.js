9ef0e536f9cc010afb6ac79c008adc864f6c93fd
HugoMichard <hugomichard@yahoo.com>
2024-05-05 15:17:54 +0200

    Updates

--- assets/Bp3z4HxI.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./BIOGrwT5.js";const o=e(""),a=e(""),u=async n=>{const r=await fetch(n+".md").then(function(t){return t.text()}).then(function(t){return t});o.value=r,a.value=n};function i(){return{getPageMarkdown:u,pathToFile:a,viewedPageMarkdown:o}}export{i as u};
